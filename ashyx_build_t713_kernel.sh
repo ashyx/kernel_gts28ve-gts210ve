f22895f3ee8fa4a124531000607e47aa33fc199e
Mark Ashworth <ashyt16@gmail.com>
2016-08-05 13:09:53 +0100

    make build script and push

---
+++ ashyx_build_t713_kernel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#BUILD SCRIPT BY ASHYX
+
+#SETUP BUILD ENVIROMENT
+export ARCH=arm64
+export CROSS_COMPILE=/home/mark/Kernels/tool-chains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+#MAKE DEFCONFIG
+make VARIANT_DEFCONFIG=msm8976_sec_gts28vewifi_eur_defconfig msm8976_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig
+cp $(pwd)/arch/arm64/configs/ashyx_gts28vewifi_defconfig $(pwd)/.config
+
+#SETUP MENU CONFIG(REMOVE # TO ENABLE)
+#make nconfig 
+
+#GET CORE COUNT AND MAKE
+CORE_COUNT=`grep processor /proc/cpuinfo|wc -l`
+make -j$CORE_COUNT
+
+#BUILD DTB
+tools/dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/
